56076f11254f1223fa96032385101a8b509606d4
Elias Pühringer <elias.puehringer@outlook.at>
2022-06-14 09:37:55 +0200

    Initial Commit

---
+++ readme.md
@@ -0,0 +1,6 @@
+# A small text adventure game made with prolog
+## Starcademy - A Star Wars based Text Adventure
+## What is this game about?
+Well basically, you are placed inside the ancient Sith Academy on Korriban. There you need to overcome a variety of Tests by your instructors and later sith master
+## When is it based?
+In the era of the Old Sith Empire/Old Republic (~3600 BBY) 
---
+++ starcademy.pl
@@ -0,0 +1,145 @@
+/* Star Academy, by Elias Pühringer, Ali Coban. */
+
+:- dynamic i_am_at/1, at/2, holding/1.
+:- retractall(at(_, _)), retractall(i_am_at(_)), retractall(alive(_)).
+
+i_am_at(mainhall).
+
+path(exit, n, mainhall).
+path(mainhall, s, exit).
+path(mainhall, e, eastcorridor).
+path(mainhall, n, northcorridor).
+path(mainhall, w, westcorridor).
+path(northcorridor, n, bedchamber).
+path(bedchamber, s, northcorridor).
+
+
+
+at(thing, mainhall).
+
+/* These rules describe how to pick up an object. */
+
+take(X) :-
+        holding(X),
+        write('You''re already holding it!'),
+        !, nl.
+
+take(X) :-
+        i_am_at(Place),
+        at(X, Place),
+        retract(at(X, Place)),
+        assert(holding(X)),
+        write('OK.'),
+        !, nl.
+
+take(_) :-
+        write('I don''t see it here.'),
+        nl.
+
+
+/* These rules describe how to put down an object. */
+
+drop(X) :-
+        holding(X),
+        i_am_at(Place),
+        retract(holding(X)),
+        assert(at(X, Place)),
+        write('OK.'),
+        !, nl.
+
+drop(_) :-
+        write('You aren''t holding it!'),
+        nl.
+
+
+/* These rules define the direction letters as calls to go/1. */
+
+n :- go(n).
+
+s :- go(s).
+
+e :- go(e).
+
+w :- go(w).
+
+
+/* This rule tells how to move in a given direction. */
+
+go(Direction) :-
+        i_am_at(Here),
+        path(Here, Direction, There),
+        retract(i_am_at(Here)),
+        assert(i_am_at(There)),
+        !, look.
+
+go(_) :-
+        write('You can''t go that way.').
+
+
+/* This rule tells how to look about you. */
+
+look :-
+        i_am_at(Place),
+        describe(Place),
+        nl,
+        notice_objects_at(Place),
+        nl.
+
+
+/* These rules set up a loop to mention all the objects
+   in your vicinity. */
+
+notice_objects_at(Place) :-
+        at(X, Place),
+        write('There is a '), write(X), write(' here.'), nl,
+        fail.
+
+notice_objects_at(_).
+
+
+/* This rule tells how to die. */
+
+die :-
+        finish.
+
+
+/* Under UNIX, the "halt." command quits Prolog but does not
+   remove the output window. On a PC, however, the window
+   disappears before the final output can be seen. Hence this
+   routine requests the user to perform the final "halt." */
+
+finish :-
+        nl,
+        write('The game is over. Please enter the "halt." command.'),
+        nl.
+
+
+/* This rule just writes out game instructions. */
+
+instructions :-
+        nl,
+        write('Enter commands using standard Prolog syntax.'), nl,
+        write('Available commands are:'), nl,
+        write('start.             -- to start the game.'), nl,
+        write('n.  s.  e.  w.     -- to go in that direction.'), nl,
+        write('take(Object).      -- to pick up an object.'), nl,
+        write('drop(Object).      -- to put down an object.'), nl,
+        write('look.              -- to look around you again.'), nl,
+        write('instructions.      -- to see this message again.'), nl,
+        write('halt.              -- to end the game and quit.'), nl,
+        nl.
+
+
+/* This rule prints out instructions and tells where you are. */
+
+start :-
+        instructions,
+        look.
+
+
+/* These rules describe the various rooms.  Depending on
+   circumstances, a room may have more than one description. */
+
+describe(mainhall) :- write('You are in the mainhall of the Sith Academy.'), nl.
+describe(northcorridor) :- write('You are in a long corridor in the north of the academy halls. North of you are the Sith Bedchambers, east at the end of the corridor is [redacted] and in the west is the instructors chamber.'), nl.
+describe(redacted) :- write('As you try to go near [redacted] a force pushes you back to '), write(i_am_at), nl.
